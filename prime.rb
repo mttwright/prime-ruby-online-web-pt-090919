c5453ca5cc35155cca075e8f1746ac7289eb55a3
virtual-frame-7789 <mttwright@gmail.com>
2019-09-21 19:28:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  if num == -1 || num == 0 || num == 1
+    return false
+  else
+  
+    range_array = (2..num - 1).to_a
+    counter = 0
+    while counter < range_array.length
+      if num % range_array[counter] == 0
+        return false
+      else
+        counter += 1
+      end
+    end
+    true
+  end
+end
+
