9e127ad401d4402fee353e1e5a9b2c4de5a183c8
virtual-frame-7789 <mttwright@gmail.com>
2019-09-21 19:21:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,22 @@
 # Add  code here!
 def prime?(num)
-  range_array = (2..num - 1).to_a
-  counter = 0
-  while counter < range_array.length
-    if num % range_array[counter] == 0
-      return false
-    else
-      counter += 1
+  if num == -1 || num == 0 || num == 1
+    return false
+  else
+  
+  
+  
+    range_array = (2..num - 1).to_a
+    counter = 0
+    while counter < range_array.length
+      if num % range_array[counter] == 0
+        return false
+      else
+        counter += 1
+      end
     end
+    true
   end
-  true
 end
 
 puts prime?(1)
